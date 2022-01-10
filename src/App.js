5d84b1b68d141111f52953abbf8fa62a9ec9878d
Viviana <vileal.88@gmail.com>
2022-01-10 17:03:04 -0300

    funcionalidad del boton de carrito para añadir

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container,} from "react-bootstrap";
 import { HeaderApp } from "./components/HeaderApp.js";
 import { ListProducts } from './components/ListProducts';
+import { AlertProduct } from './components/AlertProduct';
 const App = () => {
   return (
     <Container>
     <div className="App">
         <HeaderApp />
+        <AlertProduct />
         <ListProducts />
     </div>
     </Container>
--- src/components/ShoppingCart/ShoppingCartItem.js
+++ src/components/ShoppingCart/ShoppingCartItem.js
@@ -2,10 +2,8 @@ import React from "react";
 import { NavDropdown } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
-export const ShoppingCartItem = ({ item }) => (
-  <NavDropdown.Item>
-    <Link to={`/product/${item.id}`}>
-      {item.title} - {item.category} - ${item.price}{" "}
-    </Link>
+export const ShoppingCartItem = ({item}) => (
+  <NavDropdown.Item> 
+    {item.title} - {item.category} - ${item.price}
   </NavDropdown.Item>
 );
